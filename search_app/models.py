8dbff352696085a0c4ac3cbf236a8192870b24c3
Rohit Jain <rohitjain@Rohits-MacBook-Air.local>
2023-02-06 00:23:58 +0530

    added mapping of key value in search

--- search_app/models.py
+++ search_app/models.py
@@ -9,4 +9,28 @@ class Product(models.Model):
     subCategory = models.CharField(max_length = 100)
 
 
+    def __str__(self):
+        return self.name
+
+
+
+class DictKey(models.Model):
+    name = models.CharField(max_length = 100)
+    type = models.CharField(max_length = 100)
+
+
+    def __str__(self):
+        return self.name
+
+
+class DictKeyValue(models.Model):
+    container = models.ForeignKey(DictKey, on_delete = models.CASCADE)
+    key = models.CharField(max_length = 100)
+    value = models.CharField(max_length = 100)
+
+
+    def __str__(self):
+        return self.value
+
+
     
--- search_app/views.py
+++ search_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Product
+from .models import *
+
 
 # Create your views here.
 
@@ -10,6 +11,8 @@ def home(request):
 
 
 def editDistance(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
     a = len(str1)
     b = len(str2)
     string_matrix = [[0 for i in range(b+1)] for i in range(a+1)]
@@ -28,37 +31,86 @@ def editDistance(str1, str2):
     return string_matrix[a][b]
 
 
+##version- 1
+
+# def search(request, inputText):
+#     #print(inputText)
+
+#     ##compare with productName
+#     filterProducts = []
+
+#     threshold = 2
+
+#     products = Product.objects.all()
+#     for product in products:
+#         #compare with name
+#         distanceBetweenName = editDistance(inputText, product.name)
+#         print("distanceBetweenName ", distanceBetweenName)
+
+#         #compare with brand
+#         distanceBetweenBrand = editDistance(inputText, product.brand)
+
+#         #compare with category
+#         distanceBetweenCategory = editDistance(inputText, product.category)
+
+#         #compare with subcategory
+#         distanceBetweenSubCategory = editDistance(inputText, product.subCategory)
+
+#         if distanceBetweenName < threshold or \
+#         distanceBetweenBrand < threshold or \
+#         distanceBetweenCategory < threshold or \
+#         distanceBetweenSubCategory < threshold:
+
+#             filterProducts.append(product)
+
+
+#     return render(request, 'index.html', {'products' : filterProducts})
+
+
+##version-2
+
+
 def search(request, inputText):
-    #print(inputText)
-    
-    ##compare with productName
+    filterProductsID = set()
+
+    threshold = 2
+
+    for valueObject in DictKeyValue.objects.all():
+        distance = editDistance(inputText, valueObject.value)
+        keyObject =  valueObject.container
+        type = keyObject.type
+
+        if type.lower() == 'name':
+            if distance < threshold:
+                products = Product.objects.all().filter(name = keyObject.name)
+                for product in products:
+                    filterProductsID.add(product.id)
+
+        elif type.lower() == 'category':
+            if distance < threshold:
+                products = Product.objects.all().filter(category = keyObject.name)
+                for product in products:
+                    filterProductsID.add(product.id)
+
+        elif type.lower() == 'brand':
+            if distance < threshold:
+                products = Product.objects.all().filter(brand = keyObject.name)
+                for product in products:
+                    filterProductsID.add(product.id)
+
+
+        
+
     filterProducts = []
+    for id in filterProductsID:
+        filterProducts.append(Product.objects.get(id = id))
 
-    threshold = 4
-
-    products = Product.objects.all()
-    for product in products:
-        #compare with name
-        distanceBetweenName = editDistance(inputText, product.name)
-
-        #compare with brand
-        distanceBetweenBrand = editDistance(inputText, product.brand)
-
-        #compare with category
-        distanceBetweenCategory = editDistance(inputText, product.category)
-
-        #compare with subcategory
-        distanceBetweenSubCategory = editDistance(inputText, product.subCategory)
-
-        if distanceBetweenName < threshold or \
-        distanceBetweenBrand < threshold or \
-        distanceBetweenCategory < threshold or \
-        distanceBetweenSubCategory < threshold:
-
-            filterProducts.append(product)
 
 
     return render(request, 'index.html', {'products' : filterProducts})
+    
+
+
 
 
 
